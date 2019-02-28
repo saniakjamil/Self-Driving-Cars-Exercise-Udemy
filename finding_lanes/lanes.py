fbe6fccef592fa600055f4cf88311b1c00ba45c8
Sania K. Jamil <sania.jamil@venturedive.com>
2019-02-28 12:14:25 +0500

    Exercise 01, blur

---
+++ finding_lanes/lanes.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imshow("result", blur)
+cv2.waitKey(0)
